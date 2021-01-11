f54fcef3975d24278ee1a358adc9058c22ad85eb
sheetalbedarkar <sheetalbedarkar96@gmail.com>
2021-01-11 17:34:41 +0530

    Added: Calculating daily wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,16 +1,18 @@
-#Welcome to the employee wage computation problem statement
-
 #!/bin/bash -x
 
 #constant
 IS_PRESENT=1
+EMP_RATE_PER_HR=20
+FULL_TIME=8
 
 #variable
 empCheck=$(( RANDOM%2 ))
+dailyWage=0
 
 if [ $empCheck -eq $IS_PRESENT ]
 then
 	echo "Employee is present"
+	dailyWage=$(( $EMP_RATE_PER_HR*$FULL_TIME ))
 else
 	echo "Employee is absent"
 fi
